5058f48e2ed9dd95492a2b3f48d47f06953f6a8f
Nyasha Chizampeni <nchizampeni@gmail.com>
2023-04-02 01:57:52 +0200

    savepoint

--- certificates/signals/generate_certificates.py
+++ certificates/signals/generate_certificates.py
@@ -3,9 +3,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from certificates.models import Certificate
 from certificates.helpers import CertificateGenerator
+import random
 
 @receiver(post_save, sender=Certificate, dispatch_uid='generate_certificate')
 def generate_certificate(sender, instance, created, **kwargs):
+    
     if created:
         certificate = CertificateGenerator(instance)
         
--- certificates/signals/validate_certificate.py
+++ certificates/signals/validate_certificate.py
@@ -3,20 +3,21 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from certificates.models import Certificate, CertificateValidation
 from certificates.helpers import validate
+import random
 
 @receiver(post_save, sender=CertificateValidation, dispatch_uid='validate_certificate')
 def validate_certificate(sender, instance, created, **kwargs):
     if created:
         settings.LOGGER.critical(instance.certificate)
-        #certificate = validate(instance)
+        status = random.choice(['Clean', 'Fake'])
         certificate = Certificate.objects.first()
         
         if certificate:
-            instance.status = 'Clean'
+            instance.status = status
             instance.save()
             settings.LOGGER.success(f'{instance.title} generated successfully')
         else:
-            instance.status = 'Fake'
+            instance.status = status
             instance.save()
             settings.LOGGER.error(f'{instance.title} certificate generation failed')
         
