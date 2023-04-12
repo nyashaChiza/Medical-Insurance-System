8b9bd4ae59951283f10eb13f869b232e5c9011e3
Courage <courage@petalmafrica.com>
2023-04-12 12:55:50 +0200

    users

---
+++ account/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ("email",)
--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from account.views import UserListView, UserUpdateView, UserDeleteView
 
 urlpatterns = [
     path('', views.login_user, name="login"),
+    path('users-index', UserListView.as_view(), name="users-index"),
+    path('register-user', views.register_user, name="register-user"),
+    path('update-user/<int:pk>/', UserUpdateView.as_view(), name="update-user"),
+    path('delete-user/<int:pk>/', UserDeleteView.as_view(), name="delete-user"),
 ]
